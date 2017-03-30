d50732b67a38b4785addd9528a88ce2405291819
Shayne Holmes <shayneh@qualtrics.com>
2017-03-30 10:33:04 -0700

    Add some helpful VIM commands to make generating password macros easier

--- keyboard/ergodox/keymap_simon_passwords.example.h
+++ keyboard/ergodox/keymap_simon_passwords.example.h
@@ -1,2 +1,10 @@
 case MACRO_PASSWORD1:
     return MACRO( D(LSFT), T(H), U(LSFT), T(E), T(L), T(L), T(O), T(DOT), T(W), T(O), T(R), T(L), T(D), T(ENTER), END); // types "Hello.world{ENTER}"
+
+/*
+VIM commands to make this easier to generate
+-- Dvorak translate:
+call setline(line('.'), tr(getline('.'), 'axje.uidchtnmbrl poygk,qf;svwAXJE.UIDCHTNMBRL POYGK,QF;SVW', 'abcdefghijklmnopqrstuvwxyz;.,ABCDEFGHIJKLMNOPQRSTUVWXYZ:<>'))
+-- Macro-ify:
+s/./T(&), /g|s/T([A-Z])/D(LSFT), &, U(LSFT)/|s/[a-z]/\U&/g|norm I	return MACRO(AT(ENTER), END);
+*/
